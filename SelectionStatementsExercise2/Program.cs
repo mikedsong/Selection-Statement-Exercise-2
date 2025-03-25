5a06bf98b2b800573315ed7e1caa0c62de4b171a
michaelsong95@yahoo.com <michaelsong95@yahoo.com>
2025-03-25 11:33:53 +0900

    completed exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Threading.Channels;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I don't prefer math");
+                    break;
+                case "science":
+                    Console.WriteLine("science rules!");
+                    break;
+                case "pe":
+                case "p.e.":
+                case "physical education":
+                    Console.WriteLine("P.E. is fun!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my favorite too");
+                    break;
+                case "geography":
+                    Console.WriteLine("I like to study the world too");
+                    break;
+                default:
+                    Console.WriteLine("I like that too");
+                    break;
+                
+            }
         }
     }
 }
